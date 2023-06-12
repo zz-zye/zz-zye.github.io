bc603379f89abf2314ba48deb2397fea2d121803
zz-zye <htht2808@gmail.com>
2023-06-12 12:00:24 +0900

    add aside to projects page
    - add simple aside with scrolling h2
    - divide the projects page into 4 sections
    - change title font to space grotesk (temporary)

--- docs/projects/index.e1b434d6.js
+++ docs/projects/index.e1b434d6.js
@@ -1,2 +1,2 @@
-!function(){const e=document.querySelector("main"),t=document.getElementsByTagName("section"),n=document.getElementsByTagName("h1");t.length>=3&&n.length>=3&&e.addEventListener("scroll",(()=>{for(let o=0;o<3;o++){let l=e.scrollTop-t[o].offsetTop;n[o].style.top=l.toString()+"px"}}))}();
+!function(){const e=document.querySelector("main"),t=document.getElementsByTagName("section"),o=document.getElementsByTagName("h2");t.length>=3&&o.length>=3&&e.addEventListener("scroll",(()=>{for(let n=0;n<3;n++){let l=e.scrollTop-t[n].offsetTop-t[n].offsetHeight;o[n].style.top=l.toString()+"px"}}))}();
 //# sourceMappingURL=index.e1b434d6.js.map
--- src/projects/script.ts
+++ src/projects/script.ts
@@ -2,12 +2,13 @@
   /* add scrolling effect to titles*/
   const main = document.querySelector("main") as HTMLElement;
   const sections = document.getElementsByTagName("section");
-  const titles = document.getElementsByTagName("h1");
+  const titles = document.getElementsByTagName("h2");
 
   if (sections.length >= 3 && titles.length >= 3) {
     main.addEventListener("scroll", () => {
       for (let i = 0; i < 3; i++) {
-        let title_y = main.scrollTop - sections[i].offsetTop;
+        let title_y =
+          main.scrollTop - sections[i].offsetTop - sections[i].offsetHeight;
         titles[i].style.top = title_y.toString() + "px";
       }
     });

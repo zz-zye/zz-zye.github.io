9a1c8dd207308b0d2a9c13993bb604e32c1e04de
zz-zye <htht2808@gmail.com>
2023-07-07 02:50:10 +0900

    add parcel into package.json
    - parcel is now in devDependencies
    - changed some invalid links in html
    - temporary grainy background in projects/pattern

--- docs/projects/index.e1b434d6.js
+++ docs/projects/index.e1b434d6.js
@@ -1,2 +1,2 @@
-!function(){const e=document.querySelector("main"),t=document.getElementsByTagName("section"),o=document.getElementsByTagName("h2");t.length>=3&&o.length>=3&&e.addEventListener("scroll",(()=>{for(let n=0;n<3;n++){let l=e.scrollTop-t[n].offsetTop-t[n].offsetHeight;o[n].style.top=l.toString()+"px"}}))}();
+!function(){let e=document.querySelector("main"),t=document.getElementsByTagName("section"),o=document.getElementsByTagName("h2");t.length>=3&&o.length>=3&&e.addEventListener("scroll",()=>{for(let n=0;n<3;n++){let l=e.scrollTop-t[n].offsetTop-t[n].offsetHeight;o[n].style.top=l.toString()+"px"}})}();
 //# sourceMappingURL=index.e1b434d6.js.map
